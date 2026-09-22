fbea33f71616258cf16f3c3f51596cc4f2ba100c
Kacper <kacpi.j@interia.pl>
2026-09-22 22:40:11 +0200

    Uzupelniam_o_dodawanie_i_odejmowanie

--- calculator.py
+++ calculator.py
@@ -1,1 +1,9 @@
 print("Moj kalkulator")
+liczbax = input("podaj liczbe x:")
+liczbay = input("podaj liczbe y:")
+print("wybierz działanie: dodawanie, odejmowanie")
+działanie = input("wybierz działanie:")
+if działanie == "dodawanie":
+    print("wynik dodawania to:", int(liczbax) + int(liczbay))
+elif działanie == "odejmowanie":
+    print("wynik odejmowania to:", int(liczbax) - int(liczbay))
