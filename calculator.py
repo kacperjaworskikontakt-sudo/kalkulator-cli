ceee7d4a7b97cff34f0c45f5f04c0418a677899f
Kacper <kacpi.j@interia.pl>
2026-09-24 22:15:26 +0200

    Dodaje_bezpieczniejsza_wersje

--- calculator.py
+++ calculator.py
@@ -1,10 +1,17 @@
 print("Moj kalkulator")
 liczbax = input("podaj liczbe x:")
 liczbay = input("podaj liczbe y:")
+try:
+    int(liczbax)
+    int(liczbay)
+except ValueError:
+    print("podaj prawidłowe liczby")
+    exit()
 print("wybierz działanie: dodawanie, odejmowanie")
 działanie = input("wybierz działanie:")
+działanie = działanie.lower()
+działanie = działanie.strip()
 if działanie == "dodawanie":
     print("wynik dodawania to:", int(liczbax) + int(liczbay))
 elif działanie == "odejmowanie":
     print("wynik odejmowania to:", int(liczbax) - int(liczbay))
-print("test")
