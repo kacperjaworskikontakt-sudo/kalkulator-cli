9346294398dfe87bd45375c46bfadb4d7bc84dc4
Kacper <kacpi.j@interia.pl>
2026-09-24 22:27:28 +0200

    Dodaje_mnozenie_i_dzielenie

--- calculator.py
+++ calculator.py
@@ -5,13 +5,21 @@ try:
     int(liczbax)
     int(liczbay)
 except ValueError:
-    print("podaj prawidłowe liczby")
+    print("Nastepnym razem podaj prawidłowe liczby")
     exit()
-print("wybierz działanie: dodawanie, odejmowanie")
+print("wybierz działanie: dodawanie, odejmowanie, mnożenie, dzielenie")
 działanie = input("wybierz działanie:")
-działanie = działanie.lower()
-działanie = działanie.strip()
+działanie = działanie.lower().strip()
 if działanie == "dodawanie":
     print("wynik dodawania to:", int(liczbax) + int(liczbay))
 elif działanie == "odejmowanie":
     print("wynik odejmowania to:", int(liczbax) - int(liczbay))
+elif działanie == "mnożenie":
+    print("wynik mnożenie to:", int(liczbax) * int(liczbay))
+elif działanie == "dzielenie":
+    if int(liczbay) == 0:
+        print("Nie można dzielić przez 0")
+    else:
+        print("wynik dzielenia to", int(liczbax) / int(liczbay))
+else:
+    print("Nieprawidłowe działanie")
